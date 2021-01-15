a5f4090e3c008b61e56923cfe280576809e6fe0c
marco_p_v_vezzoli <marco.p.v.vezzoli@gmail.com>
2021-01-15 10:06:30 +0000

    added new elements

--- read_date_time/src/main.rs
+++ read_date_time/src/main.rs
@@ -16,14 +16,8 @@ fn generate_list() -> org::CheckList{
     ).collect()
   }
 }
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("received args: {:?}", args);
-    if let Some(value) = args.get(1) {
-      let rdt : Result<DateTime<Utc>,_> = Utc.datetime_from_str(value, "%Y-%m-%d %H:%M:%S");
-      match rdt{
-        Ok(dt) => {
-          println!("starting date: {}",dt.to_string());
+
+fn planning(dt:DateTime<Utc>) -> org::Node{
           let mut nodes : Vec<org::Node> = (0..30).
             map(|i| 
             org::Node {
@@ -152,11 +146,40 @@ fn main() {
           nodes.append(&mut nodes5);
           nodes.append(&mut nodes6);
           nodes.append(&mut nodes7);
-          let month = dt.format("%B %Y Planning").to_string();
-          let month_node = org::NodeBuilder::new(String::from(month)).add_children(nodes).set_todo("TODO".into()).build();
-          let planning_node = org::NodeBuilder::new("Planning".to_string()).add_children(vec![month_node]).build();
-          let root = org::NodeBuilder::new("Group".to_string()).add_children(vec![planning_node]).build();
-          root.display(1);
+          let month = dt.format("%Y %B Planning").to_string();
+          let month_node = org::NodeBuilder::new(month).add_children(nodes).set_todo("TODO".into()).build();
+          let planning_node = org::NodeBuilder::new("Planning").add_children(vec![month_node]).build();
+          let root = org::NodeBuilder::new("Group").add_children(vec![planning_node]).build();
+          root
+}
+
+fn data_analysis(dt:DateTime<Utc>) -> org::Node{
+  let arda = org::NodeBuilder::new(dt.format("Arda %Y %B"))
+  .add_children(vec![
+    org::NodeBuilder::new(dt.format("Arda Maintenance %B")).build(),
+    org::NodeBuilder::new(dt.format("Arda Meetings %B")).build()
+  ]).build();
+  let webcalc = org::NodeBuilder::new(dt.format("Web Calculators %Y %B"))
+  .add_children(vec![
+    org::NodeBuilder::new(dt.format("Web Calculators Maintenance %B")).build(),
+    org::NodeBuilder::new(dt.format("Web Calculators Meetings %B")).build()
+  ]).build();
+  let root = org::NodeBuilder::new("Data Analysis").add_children(vec![arda, webcalc]).build();
+  root
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("received args: {:?}", args);
+    if let Some(value) = args.get(1) {
+      let rdt : Result<DateTime<Utc>,_> = Utc.datetime_from_str(value, "%Y-%m-%d %H:%M:%S");
+      match rdt{
+        Ok(dt) => {
+          println!("starting date: {}",dt.to_string());
+          let plan_root = planning(dt);
+          plan_root.display(1);
+          let da_root = data_analysis(dt);
+          da_root.display(1);
         },
         Err(m) => println!("parse failed of '{}': {}",value,m)
       }
